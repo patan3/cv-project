787c82f37b54cacf01aa2f84639258168a860e8d
patan3 <62438886+patan3@users.noreply.github.com>
2026-02-12 16:38:27 +0100

    calibration runs code but bug in interpolation :S

--- corners.py
+++ corners.py
@@ -43,22 +43,44 @@ def interpolate_chessboard_corners(points, pattern_size):
             corners.append(p)
 
     corners = np.array(corners, dtype=np.float32).reshape(-1, 1, 2)
+
     return corners
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+def run_calibration(_objpoints, _imgpoints):
+    """
+    Solves for K, dist, R, t using the provided images.
+    """
+    
+    # Flags:
+    # CALIB_FIX_ASPECT_RATIO for fx != fy and CALIB_FIX_PRINCIPAL_POINT to enforce center
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
+        _objpoints, 
+        _imgpoints, 
+        gray.shape[::-1], # Image Size (width, height)
+        None, 
+        None,
+        flags=0 # Ensure fx != fy AND camera center to be estimated
+    )
 
-square_size = 0.017
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    return ret, mtx, dist, rvecs, tvecs
+
+# 3D World Points (X_world) #
+# define the board coordinates in the 3D world with z=0
+# i.e., the points are (0,0,0), (1.7,0,0), (3.4,0,0), etc.
+square_size = 0.017 # 1.7 cm in meters
 objp = np.zeros((6*9,3), np.float32)
 objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 objp *= square_size
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+# Arrays to store object points and image points from all the images for runs 1-3.
+auto_objpoints = [] # 3D points for auto-detected images
+auto_imgpoints = [] # 2D points for auto-detected images
+manual_objpoints = [] # 3D points for manually clicked images
+manual_imgpoints = [] # 2D points for manually clicked images
 
 images = glob.glob('images/*.jpg')
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 for fname in images:
     img = cv.imread(fname)
@@ -69,21 +91,26 @@ for fname in images:
 
     # If found, add object points, image points (after refining them)
     if ret == True:
-        objpoints.append(objp)
+        auto_objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners2)
+        auto_imgpoints.append(corners2)
 
         # Draw and display the corners
         cv.drawChessboardCorners(img, (9,6), corners2, ret)
+        cv.circle(img, tuple(corners2[0][0].astype(int)), 10, (0, 255, 0), -1) # Green = Start
+        cv.circle(img, tuple(corners2[-1][0].astype(int)), 10, (0, 0, 255), -1) # Red = End
         cv.imshow('img', img)
         cv.waitKey(500)
     else:
         points4 = get_four_corners_manually(img)
         corners = interpolate_chessboard_corners(points4, pattern_size=(6,9))
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners2)
+        manual_objpoints.append(objp)
+        manual_imgpoints.append(corners2)
         
         cv.drawChessboardCorners(img, (9,6), corners2, ret)
+        cv.circle(img, tuple(corners2[0][0].astype(int)), 10, (0, 255, 0), -1) # Green = Start
+        cv.circle(img, tuple(corners2[-1][0].astype(int)), 10, (0, 0, 255), -1) # Red = End
         cv.imshow('img', img)
         cv.waitKey(0)
         
@@ -99,3 +126,30 @@ images\WIN_20260211_15_54_49_Pro.jpg
 images\WIN_20260211_15_54_53_Pro.jpg
 images\WIN_20260211_15_54_57_Pro.jpg
 """
+
+
+
+# - RUN 1: Use ALL 25 images (Auto + Manual) -
+run1_obj = auto_objpoints + manual_objpoints
+run1_img = auto_imgpoints + manual_imgpoints
+
+rms1, K1, dist1, rvecs1, tvecs1 = run_calibration(run1_obj, run1_img)
+print(f"Run 1 Reprojection Error: {rms1}")
+print("K Matrix Run 1:\n", K1)
+
+# - RUN 2: Use 5 Auto + 5 Manual -
+# (we take the first 5 from auto, in total we have 7)
+run2_obj = auto_objpoints[:5] + manual_objpoints
+run2_img = auto_imgpoints[:5] + manual_imgpoints
+
+rms2, K2, dist2, rvecs2, tvecs2 = run_calibration(run2_obj, run2_img)
+print(f"Run 2 Reprojection Error: {rms2}")
+print("K Matrix Run 2:\n", K2)
+
+# - RUN 3: Use 5 Auto -
+run3_obj = auto_objpoints[:5]
+run3_img = auto_imgpoints[:5]
+
+rms3, K3, dist3, rvecs3, tvecs3 = run_calibration(run3_obj, run3_img)
+print(f"Run 3 Reprojection Error: {rms3}")
+print("K Matrix Run 3:\n", K3)
